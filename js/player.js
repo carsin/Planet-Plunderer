61e2b866c37f54fcb14a0328fc72e5f42796b3ab
Carson <carsonfreedman1@gmail.com>
2017-04-22 09:13:05 -1000

    Layout 1
    Revert to this if new layout fails

---
+++ js/player.js
@@ -0,0 +1,14 @@
+var player = {
+	businessname: undefined,
+	money: 10000,
+
+	setBusinessName: function() {
+		//player.businessname = prompt("What shall you call your buisness name");
+		player.businessname = "prompt industries";
+		$("#player-name").html(player.businessname);
+	},
+
+	updateMoney: function() {
+		$("#money-count").html(player.money);
+	},
+}
